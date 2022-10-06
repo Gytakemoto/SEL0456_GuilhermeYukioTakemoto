22b56b1a90f387064008c385440c1c64e815d97d
Gytakemoto <gytakemoto@usp.br>
2022-10-06 12:21:14 -0300

    deletando os arquivos do remoto

--- fib.c
+++
@@ -1,7 +0,0 @@
-#include "fib.h"
-
-unsigned int fib(unsigned int n){
-if (n == 0 || n == 1)
-    return 1;
-  return fib(n - 1) + fib(n-2);
-}
--- fib.h
+++
@@ -1,6 +0,0 @@
-#ifndef FIB_H
-#define FIB_H
-
-unsigned int fib(unsigned int n);
-
-#endif /*FIB_H */
