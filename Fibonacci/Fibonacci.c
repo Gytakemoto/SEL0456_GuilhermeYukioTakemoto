a9c637ec0c4382ce0e6a2793dff7a79010aff8cb
Gytakemoto <gytakemoto@usp.br>
2022-10-06 12:19:22 -0300

    Separando as pastas

---
+++ Fibonacci/Fibonacci.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "fib.h"
+
+int main(){
+unsigned int n;
+int fibonacci;
+
+printf("Entre com o valor de n: \n");
+scanf("%d", &n);
+
+fibonacci = fib(n);
+
+printf("O valor do Fibonacci eh: %d \n", fibonacci);
+return 0;
+}
+
